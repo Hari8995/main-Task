02fbd9973d739d022cd2f316b683bb03e6380542
Hari8995 <hari8995p@gmail.com>
2024-03-21 12:49:35 +0530

    first commit

--- mergesortedarray.js
+++ mergesortedarray.js
@@ -1,12 +1,32 @@
 
 
-function sor(a,b){
-    let c=a.concat(b);
-    console.log(c);
-    console.log(c.sort(function(x,y){
-        return(x-y)}))
-}
+// function sor(a,b){
+//     let c=a.concat(b);
+//     console.log(c);
+//     console.log(c.sort(function(x,y){
+//         return(x-y)}))
+// }
 
 
 
-sor([1,3,2,4],[9,8,7,6]);
+// sor([1,3,2,4],[9,8,7,6]);
+
+
+
+
+// try {
+// console.log("hello");
+// let a=20
+// let b=20
+// if(a===b){throw new  "the value is not write"}  
+
+    
+// } catch (error) {
+
+//     console.log(error);
+    
+// }finally{
+//     console.log("helloworld");
+// }
+
+// console.log("stop");
